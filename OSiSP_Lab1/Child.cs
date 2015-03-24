4701eab4f6ae020bcaeea46342c4cbf38e0e2854
Kate Lobanova <katyekaa@gmail.com>
2015-03-24 19:00:21 +0300

    Lab1

---
+++ OSiSP_Lab1/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSiSP_Lab1
+{
+    public class Child: Base<string>
+    {
+        public string id;
+        public string GetID()
+        {
+            return id;
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OSiSP_Lab1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IContravariation<Child> obj = new Contravariation<Base<string>>();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ICovariation<Base<string>> obj = new Covariation<Child>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Child))]
+        public void TestMethod3()
+        {
+            IContravariation<Child> obj = new Contravariation<Base<string>>();
+            obj = (Contravariation<Child>)obj;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Child))]
+        public void TestMethod4()
+        {
+            ICovariation<Base<string>> obj = new Covariation<Child>();
+            obj = (Covariation<Child>)obj;
+        }
+    }
+}
